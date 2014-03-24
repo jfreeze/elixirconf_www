bae391bd671059201b4b5a7d4cc1b0f35a55e00a
Jim Freeze <jimfreeze@gmail.com>
2014-03-23 19:55:48 -0500

    changed title

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -13,7 +13,7 @@ defmodule ApplicationRouter do
   # forward "/posts", to: PostsRouter
 
   get "/" do
-    conn = conn.assign(:title, "Welcome to Dynamo!")
+    conn = conn.assign(:title, "ElixirConf 2014")
     render conn, "index.html"
   end
 end
